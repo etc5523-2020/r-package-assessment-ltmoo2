ddc024d4f4ebd7c1a93befddba8bf0f791f3cdd1
Lachlan Moody <ltmoo2@student.monash.edu>
2020-10-14 18:14:28 +1100

    Added first data set required for shiny app

---
+++ R/usa_covid_data.R
@@ -0,0 +1,9 @@
+#' Daily COVID19 cases and deaths in the USA by state
+#'
+#' This dataset contains COVID19 related data from 60 states and territories of the USA from the 22nd of January to the 28th of September 2020. In total there are 15,060 rows across 13 variables.
+#'
+#' @source 
+#' Centers for Disease Control and Prevention
+#'
+
+"usa_covid_data"
