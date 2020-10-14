3e9147c8ee4cabb3c63dcbe367b7782715b91abd
Lachlan Moody <ltmoo2@student.monash.edu>
2020-10-14 19:25:04 +1100

    Added data for map plot

---
+++ R/usa_state_map.R
@@ -0,0 +1,9 @@
+#' State borders for the continental United States
+#'
+#' This dataset contains spatial data provided in a tabular format, producing a map of the United States mainland and is loaded directly from the maps package.
+#'
+#' @source 
+#' US Department of the Census
+#'
+
+"usa_state_map"
